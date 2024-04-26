5487577772a2bd78da7f394434d4cd5b61e0c41b
Xiuh185 <108249217+Xiuh185@users.noreply.github.com>
2024-04-25 18:34:06 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,3 @@
 <?php
-function conexionphp(){
-$server = "localhost";
- $user = "root";
- $pass = "Herrera02";
- $db = "technologies lh";
- $conectar = mysqli_connect ($server, $user, $pass, $db) or die ("Error en la conexión");
- return $conectar;
-}
+$conex = mysqli_connect("localhost","root","Herrera02","registro");
 ?>
