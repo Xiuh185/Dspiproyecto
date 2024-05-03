ae4f6dd6bae68476c810953d6a09795ef40f6b8d
Xiuh185 <108249217+Xiuh185@users.noreply.github.com>
2024-05-03 16:19:22 -0600

    Update consultar.php

--- consultar.php
+++ consultar.php
@@ -1,3 +1,18 @@
 <?php
-    include ("conexion.php");
+   require_once ('conexion.php');
+        $user = $_POST['username'];
+        $password = $_POST['key_username'];
+        
+        //Consulta de la base de datos
+        $sql = "SELECT * FROM datos WHERE nombre = '$user' AND contrasena = '$password'";
+        $resultado = $conex->query($sql);
+        $row = $resultado->fetch_assoc();
+
+        if($resultado->num_rows> 0){
+            session_start();
+            $_SESSION['username'] = $user;
+            header("location:tablas.php");
+        } else{
+            header("location:index.php");
+        }
 ?>
