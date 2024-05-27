1970529f3e9d9f8f709f535b30eb0fc40e924ffd
Xiuh185 <108249217+Xiuh185@users.noreply.github.com>
2024-05-26 22:41:03 -0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,12 +9,15 @@
         exit;
     }
     if(isset($_POST["delete"]) && isset($_POST["deleteId"])) {
-       
-        foreach($_POST["deleteId"] as $deleteid){
-            $delete = "DELETE FROM informes WHERE ID_Informe = $deleteid";
-            mysqli_query($conex, $delete);
-        }
-    }
+        echo ("<script>
+        alert('¿Desea eliminar estos registros?');
+        location.assign('delete.php');
+        </script>");
+         foreach($_POST["deleteId"] as $deleteid){
+             $delete = "DELETE FROM informes WHERE ID_Informe = $deleteid";
+             mysqli_query($conex, $delete);
+         }
+     }
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -40,13 +43,38 @@
 </div>
 <div class="container">
     <h2 class="H2S"> Eliminar Registros </h2>
-    <p>Por favor, seleccione uno o varios registros para poder eliminarlos:</p>
-<table class="table">
+    <h3 class="H3S">Búsqueda personalizada</h2>
+    <form class="form" method="post" action="<?=$_SERVER['PHP_SELF']?>">
+        <div class="container-form">
+            <label class="control-label">Nombre del documento:</label>
+            <input type="text" class = "form-control" name="nombresearch" placeholder="Nombre del documento">
+        </div>
+        <div class="container-form">
+            <label class="control-label">Fecha de emisión:</label>
+            <input type="date" class = "form-control" value="" name="datesearch">
+</div>
+        <div class="container-form">
+            <label class="control-label">Tipo de documento:</label>
+        <select class="form-control" name="docsearch">
+        <option value="" selected>Tipo de documento:</option>
+        <option value="DOCX">DOCX</option>
+        <option value="PDF">PDF</option>
+        <option value="XSLX">XSLX</option>
+        </select>
+        </div>
+        <div class="container-form">
+        <button type="submit" name="find" class="search">Buscar</button>
+        </div>
+</form>
+    <div class="container-form">
+        <p>Por favor, seleccione uno o varios registros para poder eliminarlos:</p>
+</div>
+    <table class="table">
     <form action="" method="post">
                 <thead  class= "table-dark">
                 <tr>
                     <td><button type="submit" class="btn" name="delete">Eliminar</button></td>
-                    <td>Id<i class="bi bi-arrow-down-up"></i></td>
+                    <td>ID<i class="bi bi-arrow-down-up"></i></td>
                     <td>Nombre de registro <i class="bi bi-arrow-down-up"></i></td>
                     <td>Fecha de emisión <i class="bi bi-arrow-down-up"></i></td>
                     <td>Tipo de documento <i class="bi bi-arrow-down-up"></i></td>
@@ -55,19 +83,71 @@
                 </thead>
                 <tbody>
                 <?php
-                $rows = mysqli_query($conex, "SELECT * FROM informes");
                 $i = 1;
+                ////////////////////////////////////////////////////////////////
+                    if (isset($_POST['find'])){
+                        $nombresearch = $_POST['nombresearch'];
+                        $datesearch = $_POST['datesearch'];
+                        $docsearch = $_POST['docsearch'];
+            
+                        if(empty($_POST['nombresearch']) && empty($_POST['datesearch']) 
+                        && empty($_POST['docsearch'])){
+                            echo ("<script>
+                            alert('Debe ingresar al menos un criterio de búsqueda');
+                            location.assign('delete.php');
+                            </script>");
+                        } else{
+                        if(!empty($_POST['nombresearch'])){
+                            $sql = "SELECT * FROM informes WHERE Nom_Informe LIKE '%$nombresearch%'";
+                        }
+                        if(!empty($_POST['datesearch'])){
+                            $sql = "SELECT * FROM informes WHERE Fecha LIKE '%$datesearch%'";
+                        }
+                        if(!empty($_POST['docsearch'])){
+                            $sql = "SELECT * FROM informes WHERE Tipo LIKE '%$docsearch%'";
+                        }
+                        if(!empty($_POST['nombresearch']) && !empty($_POST['docsearch'])){
+                            $sql = "SELECT * FROM informes WHERE Nom_Informe LIKE '%$nombresearch%' AND Tipo LIKE '%$docsearch%'";
+                        }
+                        if(!empty($_POST['datesearch']) &&!empty($_POST['docsearch'])){
+                            $sql = "SELECT * FROM informes WHERE Fecha LIKE '%$datesearch%' AND Tipo LIKE '%$docsearch%'";
+                        }
+                        if(!empty($_POST['nombresearch']) &&!empty($_POST['datesearch'])){
+                            $sql = "SELECT * FROM informes WHERE Nom_Informe LIKE '%$nombresearch%' AND Fecha LIKE '%$datesearch%'";
+                        }
+                        if(!empty($_POST['nombresearch']) &&!empty($_POST['datesearch']) &&!empty($_POST['docsearch'])){
+                            $sql = "SELECT * FROM informes WHERE Nom_Informe LIKE '%$nombresearch%' AND Fecha LIKE '%$datesearch%' AND Tipo LIKE '%$docsearch%'";
+                        }
+                        $rows = mysqli_query($conex, $sql);
+                        foreach($rows as $row) :
+                           echo '<tr>
+                           <td>' . $i++ . ' <input type="checkbox" name="deleteId[]" value="' . $row['ID_Informe'] . '"></td>
+                           <td>' . $row['ID_Informe'] . '</td>
+                           <td>' . $row['Nom_Informe'] . '</td>
+                           <td>' . $row['Fecha'] . '</td>
+                           <td>' . $row['Tipo'] . '</td>
+                           <td><a href="' . $row['Link_Download'] . '"><i class="bi bi-download"></i></a></td>
+                           </tr>';
+                        endforeach;
+                        }
+                ?>
+                <?php
+             }else{
+                $sql = "SELECT * FROM informes";
+                $rows = mysqli_query($conex, $sql);
                 foreach ($rows as $row) :
                 ?>
                 <tr>
-                    <td><input type="checkbox" name="deleteId[]" value="<?php echo $row['ID_Informe'];?>"></td>
-                    <td><?php echo $i++; ?></td>
+                    <td><?php echo $i++; ?> <input type="checkbox" name="deleteId[]" value="<?php echo $row['ID_Informe'];?>"></td>
+                    <td><?php echo $row['ID_Informe'];?></td>
                     <td><?php echo $row['Nom_Informe'];?></td>
                     <td><?php echo $row['Fecha'];?></td>
                     <td><?php echo $row['Tipo'];?></td>
                     <td><a href= "<?php echo $row['Link_Download'];?>"><i class="bi bi-download"></i></a></td>
                 </tr>
-                <?php endforeach; ?>
+                <?php endforeach; 
+                }
+                ?>
             </tbody>
             </table>
             <div class="table_fotter">
